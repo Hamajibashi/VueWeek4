2768fd9248848d1b4c13630ff3a32c83bd1e08f7
Mylène <andreasakata@gmail.com>
2022-03-11 01:21:28 +0800

    建立分頁元件

--- products.js
+++ products.js
@@ -1,97 +1,105 @@
-import {createApp} from 'https://cdnjs.cloudflare.com/ajax/libs/vue/3.2.29/vue.esm-browser.prod.min.js';
+import { createApp } from 'https://cdnjs.cloudflare.com/ajax/libs/vue/3.2.29/vue.esm-browser.prod.min.js';
+import pagination from './pagination.js';
 
 let productModal = '';
-let delModal='';
+let delModal = '';
 
 const app = {
-    data(){
-        return{
-            apiUrl:'https://vue3-course-api.hexschool.io/v2',
-            apiPath:'mylmii',
-            products:[],
-            isNew:false, //用來分辨是新增或編輯模式
-            tempProduct:{
-                imagesUrl:[],
-                },
-            }
-        },
-    methods:{
-        checkLogin(){ 
+    components:{
+        pagination
+    },
+    data() {
+        return {
+            apiUrl: 'https://vue3-course-api.hexschool.io/v2',
+            apiPath: 'mylmii',
+            products: [],
+            isNew: false, //用來分辨是新增或編輯模式
+            tempProduct: {
+                imagesUrl: [],
+            },
+            //存放 pagination 資訊
+            pagination: {},
+        }
+    },
+    methods: {
+        checkLogin() {
             // 確認是否登入
-                const url = `${this.apiUrl}/api/user/check`
-                axios.post(url)
-                .then(res=>{
-                  this.getProduct();
+            const url = `${this.apiUrl}/api/user/check`
+            axios.post(url)
+                .then(res => {
+                    this.getProduct();
                 })
-                .catch(err=>{
-                  alert(err.data.message);
-                  window.location="index.html";
+                .catch(err => {
+                    alert(err.data.message);
+                    window.location = "index.html";
                 })
         },
-        getProduct(){
-            const url = `${this.apiUrl}/api/${this.apiPath}/admin/products`;
+        getProduct(page = 1) { //參數預設值，讓預設為第一頁
+            //query 的用法為 ?page=頁數
+            const url = `${this.apiUrl}/api/${this.apiPath}/admin/products?page=${page}`;
             axios.get(url)
-                .then(res=>{
+                .then(res => {
                     this.products = res.data.products;
+                    this.pagination = res.data.pagination;
                 })
-                .catch(err=>{
+                .catch(err => {
                     alert(err.data.message);
                 })
         },
-        updateProduct(){
+        updateProduct() {
             let url = `${this.apiUrl}/api/${this.apiPath}/admin/product`;
             let httpMethod = 'post';
-        
+
             //根據 isNew 來判斷要串接 post 還是 put 
-            if(!this.isNew){
-            //編輯狀態
+            if (!this.isNew) {
+                //編輯狀態
                 url = `${this.apiUrl}/api/${this.apiPath}/admin/product/${this.tempProduct.id}`;
                 httpMethod = 'put';
             }
             // post 和 put 要帶的參數相同，整體架構也相同，所以可以寫在一起
-            axios[httpMethod](url,{data:this.tempProduct})
-                .then(res=>{
+            axios[httpMethod](url, { data: this.tempProduct })
+                .then(res => {
                     alert(res.data.message);
                     productModal.hide();
                     this.getProduct();
                 })
-                .catch(err=>{
+                .catch(err => {
                     alert(err.data.message);
                 })
         },
-        delProduct(){
+        delProduct() {
             const url = `${this.apiUrl}/api/${this.apiPath}/admin/product/${this.tempProduct.id}`;
             axios.delete(url)
-                .then(res=>{
+                .then(res => {
                     alert(res.data.message);
                     delModal.hide();
                     this.getProduct();
                 })
-                .catch(err=>{
+                .catch(err => {
                     alert(err.data.message);
                 })
         },
-        openModal(isNew, item){
-            if(isNew === 'new'){
+        openModal(isNew, item) {
+            if (isNew === 'new') {
                 this.tempProduct = { //清空物件
-                    imagesUrl:[],
+                    imagesUrl: [],
                 };
                 this.isNew = true; //新增模式
                 productModal.show();
             }
-            else if(isNew === 'edit'){
+            else if (isNew === 'edit') {
                 //使用淺拷貝避免外層物件一起更動，以及避免單圖產品無法追加圖片
-                this.tempProduct = {imagesUrl:[],...item};
+                this.tempProduct = { imagesUrl: [], ...item };
                 this.isNew = false; //編輯模式
                 productModal.show();
             }
-            else if(isNew === 'delete'){
-                this.tempProduct = {...item}; //為了能夠帶上品項名稱
-                delModal.show();	
+            else if (isNew === 'delete') {
+                this.tempProduct = { ...item }; //為了能夠帶上品項名稱
+                delModal.show();
             }
         }
     },
-    mounted(){
+    mounted() {
         // 取得 Token
         const token = document.cookie.replace(/(?:(?:^|.*;\s*)mylToken\s*\=\s*([^;]*).*$)|^.*$/, "$1");
         axios.defaults.headers.common['Authorization'] = token;
@@ -99,10 +107,10 @@ const app = {
 
         //使用 new 建立 Modal，拿到實體 DOM 並賦予到變數上
         //新增與編輯用 Modal，禁止使用者用 Esc 關閉 Modal
-        productModal = new bootstrap.Modal(document.getElementById('productModal'),{keyboard:false});
+        productModal = new bootstrap.Modal(document.getElementById('productModal'), { keyboard: false });
 
         //刪除用的 Modal，禁止使用者用鍵盤關閉 Modal
-        delModal = new bootstrap.Modal(document.getElementById('delProductModal'),{keyboard:false});
+        delModal = new bootstrap.Modal(document.getElementById('delProductModal'), { keyboard: false });
     }
 }
 
